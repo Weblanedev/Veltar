6e5b327b9038828e72b50212c9bbb801ff6e3a2c
Bunmi Oye <bunmioye09@gmail.com>
2025-01-28 13:28:09 +0100

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ const Home = () => {
         <WrapperStyleOne>
           <main>
             <HomeMain />
+            {/* note */}
             {/* <ProductModal /> */}
           </main>
         </WrapperStyleOne>
